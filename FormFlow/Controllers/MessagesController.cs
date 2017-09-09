6ce26918c04882ecb99469d89d1b59b9c5b43b73
Artyom Shukanov <shukanov.artyom@gmail.com>
2017-09-09 13:39:28 +0300

    Exploring chatbot with FormFlow

---
+++ FormFlow/Controllers/MessagesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using HelloBot.Forms;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+
+namespace HelloBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                IDialog<MedicalQuestionForm> dialog = CreateDialogFromForm();
+                await Conversation.SendAsync(activity, () => dialog);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private IDialog<MedicalQuestionForm> CreateDialogFromForm()
+        {
+            return Chain.From(
+                () => FormDialog.FromForm(
+                    () => new MedicalQuestionFormBuilder().BuildForm()));
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                throw new NotImplementedException();
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                throw new NotImplementedException();
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                throw new NotImplementedException();
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                throw new NotImplementedException();
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+                throw new NotImplementedException();
+            }
+            return null;
+        }
+    }
+}
---
+++ FormFlow/Forms/MedicalQuestionFormBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace HelloBot.Forms
+{
+    public class MedicalQuestionFormBuilder
+    {
+        public IForm<MedicalQuestionForm> BuildForm()
+        {
+            return new FormBuilder<MedicalQuestionForm>()
+                .Message("Welcome. Please answer a couple of questions to pass a test.")
+                .Build();
+        }
+    }
+}
